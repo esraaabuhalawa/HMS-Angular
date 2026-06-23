7c5344be28a4c5826a5ced9041cd65be8d0a5610
Mariam <MariamSabry1@GitHub>
2026-06-23 03:03:16 +0300

    add-edit-new

--- src/app/features/Dashboard/modules/rooms/components/add-edit/add-edit.component.ts
+++ src/app/features/Dashboard/modules/rooms/components/add-edit/add-edit.component.ts
@@ -4,6 +4,9 @@ import { FileUploadModule } from 'primeng/fileupload';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { SelectModule } from 'primeng/select';
+import { RoomsService } from '../../services/rooms.service';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
@@ -12,13 +15,20 @@ import { SelectModule } from 'primeng/select';
   InputTextModule,
   InputNumberModule,
   SelectModule,
-  FileUploadModule],
+  FileUploadModule,
+    CommonModule],
   templateUrl: './add-edit.component.html',
   styleUrl: './add-edit.component.scss',
 })
 export class AddEditComponent {
 
   private fb = inject(FormBuilder);
+  private roomsService = inject(RoomsService);
+
+
+   loading = false;
+  previewUrls: string[] = [];
+
 
 roomForm = this.fb.group({
   roomNumber: [''],
@@ -33,5 +43,46 @@ facilities = [
   { id: 2, name: 'TV' },
   { id: 3, name: 'AC' }
 ];
+
+selectedFiles: File[] = [];
+
+onSelectFiles(event: any) {
+  console.log(event);
+
+  const files = event.currentFiles || event.files;
+
+  this.selectedFiles = files;
+
+  this.previewUrls = files.map((file: File) =>
+    URL.createObjectURL(file)
+  );
+}
+
+
+ save() {
+    this.loading = true;
+
+    const formData = new FormData();
+
+    formData.append('roomNumber', this.roomForm.value.roomNumber ?? '');
+    formData.append('price', String(this.roomForm.value.price ?? ''));
+    formData.append('capacity', String(this.roomForm.value.capacity ?? ''));
+    formData.append('discount', String(this.roomForm.value.discount ?? ''));
+    formData.append('facility', String(this.roomForm.value.facility ?? ''));
+
+    this.selectedFiles.forEach(file => {
+      formData.append('images', file);
+    });
+
+    this.roomsService.createRoom(formData)
+      .subscribe({
+        next: () => {
+          this.loading = false;
+        },
+        error: () => {
+          this.loading = false;
+        }
+      });
+  }
 
 }
--- src/app/features/Dashboard/modules/rooms/services/rooms.service.ts
+++ src/app/features/Dashboard/modules/rooms/services/rooms.service.ts
@@ -1,10 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RoomsService {
   private http = inject(HttpClient);
+
+    private baseUrl = `${environment.apiUrl}/rooms`;
+
+  createRoom(data: FormData) {
+    return this.http.post(this.baseUrl, data);
+  }
+
+  getRooms() {
+    return this.http.get(this.baseUrl);
+  }
+
 
 }
