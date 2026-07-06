5e6a731dfa42124f0d66c009331aab0246b59da7
Mariam <MariamSabry1@GitHub>
2026-07-06 06:28:05 +0300

    add (explore page)

--- src/app/features/Website/modules/rooms/components/rooms-list/rooms-list.component.ts
+++ src/app/features/Website/modules/rooms/components/rooms-list/rooms-list.component.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { WebsitePageHeadingComponent } from '../../../../../../shared/components/website/ui/website-page-heading/website-page-heading.component';
+import { RoomsService } from '../../services/rooms.service';
+import { IRoom } from '../../../../../../shared/interfaces/general.interface';
+import { CardSkeltonComponent } from '../../../../../../shared/components/website/ui/card-skelton/card-skelton.component';
+import { RoomInfoCardComponent } from '../../../../../../shared/components/website/ui/room-info-card/room-info-card.component';
+import { finalize } from 'rxjs';
+import { PaginatorState, Paginator } from 'primeng/paginator';
 
 @Component({
   selector: 'app-rooms-list',
-  imports: [],
+  imports: [WebsitePageHeadingComponent, CardSkeltonComponent, RoomInfoCardComponent, Paginator],
   templateUrl: './rooms-list.component.html',
   styleUrl: './rooms-list.component.scss',
 })
-export class RoomsListComponent {}
+export class RoomsListComponent {
+  private roomsService = inject(RoomsService);
+
+  roomsList = signal<IRoom[]>([]);
+  isLoading = signal(false);
+
+  currentPage = 1;
+  pageSize = 10;
+  totalRecords = 0;
+
+  home: MenuItem = {
+    label: 'Home',
+    routerLink: '/',
+  };
+
+  breadcrumbs: MenuItem[] = [
+    {
+      label: 'Explore',
+    },
+  ];
+
+  fetchRooms() {
+    this.isLoading.set(true);
+
+    this.roomsService
+      .getAllRooms({
+        page: this.currentPage,
+        size: this.pageSize,
+      })
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: (res) => {
+          this.roomsList.set(res.data.rooms);
+          this.totalRecords = res.data.totalCount;
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+  }
+
+  ngOnInit(): void {
+    this.fetchRooms();
+  }
+
+  onPageChange(event: PaginatorState) {
+    this.currentPage = (event.page ?? 0) + 1;
+    this.pageSize = event.rows ?? 10;
+
+    this.fetchRooms();
+  }
+}
